a6a8dd0eef4f7ff553fc2c558f19b2fd958f26db
sbrumley <sbrumley@wireless-town.com>
2016-10-23 10:57:48 -0400

    Added Index Check function to package

--- govendor/src/github.com/scottbrumley/test/test.go
+++ govendor/src/github.com/scottbrumley/test/test.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"github.com/scottbrumley/elasticsearch"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Beging Testing")
+	fmt.Println("")
 	myParms := elasticsearch.GetParams()
-	elasticsearch.ConnectES(myParms)
+
+	fmt.Println("Test Connection ...")
+	resp, _ := elasticsearch.ConnectES(myParms)
+	if ( strings.Contains(resp.Status,"200") ){
+		fmt.Println("Connection Success")
+		fmt.Println("")
+		//fmt.Println(respStr)
+	} else {
+		fmt.Println("Connection Failed " + resp.Status)
+	}
+
 	indexStr := "wink"
+	fmt.Println("Test Index Exists ...")
 	if (elasticsearch.IndexExists(myParms,indexStr)){
 		fmt.Println("Index " + indexStr + " exists")
 	} else {
